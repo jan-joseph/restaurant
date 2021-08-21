e9f1b9a5260486c30453aa6a976a11ab323d5a4f
Jan Joseph <jan00joseph@gmail.com>
2021-08-22 00:35:56 +0530

    Update the build file

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,10 @@
 "use strict";
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 var path = require("path");
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin(_defineProperty({
-    title: 'Output Management'
-  }, "title", 'Development'))],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -22,8 +17,5 @@ module.exports = {
       test: /\.scss$/i,
       use: ['style-loader', 'css-loader', 'sass-loader']
     }]
-  },
-  performance: {
-    hints: false
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,6 @@ const path = require("path")
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            title: 'Development',
-        }),
-    ],
     output: {
         filename : 'main.js',
         path : path.resolve(__dirname,'dist'),
@@ -23,8 +17,5 @@ module.exports = {
                 use: ['style-loader','css-loader','sass-loader'],
             },
         ],
-    },
-    performance: {
-        hints:false,
     },
 };
